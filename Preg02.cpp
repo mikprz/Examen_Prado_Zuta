d1118cd6161f960ec8bce0206c3b59a482a6b039
Diego Quispe <diego.qc2013@gmail.com>
2022-10-12 09:45:37 -0500

    Pregunta02 - Listo

---
+++ Preg02.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+ 
+ int main () {
+ 
+ int n,suma;
+ 
+ cout<<"------Suma los n primeros numero naturales------"<<endl;
+ cout<<"-Ingrese n: ";
+ cin>>n;
+ 
+ suma=n*(n+1);
+ suma=suma/2;
+ 
+ 
+ cout<<endl<<"La suma de los  "<<n<<"  primeros numeros naturales es "<<suma;
+ 
+ 
+}
